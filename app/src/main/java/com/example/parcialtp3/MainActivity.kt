c92e81d38c2e5ff57e03e96aaf49429d8d14c7d8
Micaela3533 <micaelazarat123@gmail.com>
2023-11-01 08:40:46 -0300

    mica

--- app/src/main/java/com/example/parcialtp3/MainActivity.kt
+++ app/src/main/java/com/example/parcialtp3/MainActivity.kt
@@ -6,8 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-
+        //pueba
 
         setContentView(R.layout.activity_main)
     }
