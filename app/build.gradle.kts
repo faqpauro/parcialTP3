d6d3415e4ca398fd62a87516ba584029312af12d
faqpauro <facundopauro@gmail.com>
2023-11-02 18:25:29 -0300

    Facu Pauro: arreglos xml de Perfil y Configuracion

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.parcialtp3"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.parcialtp3"
--- app/src/main/java/com/example/parcialtp3/ConfiguracionFragment.kt
+++ app/src/main/java/com/example/parcialtp3/ConfiguracionFragment.kt
@@ -1,59 +1,55 @@
 package com.example.parcialtp3
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Switch
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ConfiguracionFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ConfiguracionFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_configuracion, container, false)
+        val view = inflater.inflate(R.layout.fragment_configuracion, container, false)
+
+        val darkModeSwitch: Switch = view.findViewById(R.id.darkModeSwitch)
+        darkModeSwitch.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                // activar dark mode
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                saveDarkModeState(true)
+            } else {
+                // desactivar dark mode
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                saveDarkModeState(false)
+            }
+        }
+
+        // Initialize the switch position based on the saved state
+        darkModeSwitch.isChecked = loadDarkModeState()
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ConfiguracionFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ConfiguracionFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun saveDarkModeState(isDarkMode: Boolean) {
+        val sharedPreferences = requireActivity().getPreferences(Context.MODE_PRIVATE)
+        with(sharedPreferences.edit()) {
+            putBoolean("DARK_MODE", isDarkMode)
+            apply()
+        }
     }
+
+    private fun loadDarkModeState(): Boolean {
+        val sharedPreferences = requireActivity().getPreferences(Context.MODE_PRIVATE)
+        return sharedPreferences.getBoolean("DARK_MODE", false)
+    }
+
 }
