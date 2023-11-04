2f24f793fa7e8f1df2a6173bc300386fc676b23e
Tomas <tflorimo@gmail.com>
2023-11-03 21:02:56 -0300

    fix conflicts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,11 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.parcialtp3"
-<<<<<<< HEAD
     compileSdk = 34
-=======
-    compileSdk = 34 //cambiar por 33
->>>>>>> dev
 
     defaultConfig {
         applicationId = "com.example.parcialtp3"
@@ -56,19 +52,16 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-<<<<<<< HEAD
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-
     implementation("androidx.room:room-runtime:$room_version")
     annotationProcessor("androidx.room:room-compiler:$room_version")
     kapt("androidx.room:room-compiler:$room_version")
     // soporte corutinas
     implementation("androidx.room:room-ktx:$room_version")
     kapt("androidx.room:room-compiler:$room_version")
-=======
     implementation("de.hdodenhof:circleimageview:3.1.0")
     implementation("androidx.drawerlayout:drawerlayout:1.2.0")
->>>>>>> dev
+
 
 }
