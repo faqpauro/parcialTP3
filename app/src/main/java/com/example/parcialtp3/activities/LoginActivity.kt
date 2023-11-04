3d2d1838093b204a59195ee07376c132afa5353f
Micaela3533 <micaelazarat123@gmail.com>
2023-11-04 20:11:46 -0300

    login, items mascotas y filtro

---
+++ app/src/main/java/com/example/parcialtp3/activities/LoginActivity.kt
@@ -0,0 +1,13 @@
+
+package com.example.parcialtp3.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.parcialtp3.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
