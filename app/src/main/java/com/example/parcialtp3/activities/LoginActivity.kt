85703ccecef77c3cf01a7e72c24ac3c1817e4c33
Micaela3533 <micaelazarat123@gmail.com>
2023-11-05 17:44:42 -0300

    De inicio a logi

--- app/src/main/java/com/example/parcialtp3/activities/LoginActivity.kt
+++ app/src/main/java/com/example/parcialtp3/activities/LoginActivity.kt
@@ -13,7 +13,6 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-
         val isDarkMode = sharedViewModel.getDarkModeState(this)
         if (isDarkMode) {
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
