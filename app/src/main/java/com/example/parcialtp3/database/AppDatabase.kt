e7c61c5a409b598c2a445d82fffd31a77f5f57ea
faqpauro <facundopauro@gmail.com>
2023-12-02 04:45:33 -0300

    Facu Pauro: ajustes varios

--- app/src/main/java/com/example/parcialtp3/database/AppDatabase.kt
+++ app/src/main/java/com/example/parcialtp3/database/AppDatabase.kt
@@ -5,15 +5,13 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
-import androidx.room.migration.Migration
-import androidx.sqlite.db.SupportSQLiteDatabase
 import com.example.parcialtp3.entities.Adoption
 import com.example.parcialtp3.entities.DateConverter
 import com.example.parcialtp3.entities.Dog
 import com.example.parcialtp3.entities.User
 import com.example.parcialtp3.entities.UserFavorite
 
-@Database(entities = [User::class, Dog::class, Adoption::class, UserFavorite::class], version = 2, exportSchema = false)
+@Database(entities = [User::class, Dog::class, Adoption::class, UserFavorite::class], version = 3, exportSchema = false)
 @TypeConverters(DateConverter::class)
 abstract class AppDatabase : RoomDatabase() {
 
